4259129c5fa06ea12eb3338b0d650eb10d0f1b2f
“hhzh <huzihao79@163.com>
2017-09-22 17:09:10 +0800

    .

--- REID_2.py
+++ REID_2.py
@@ -51,8 +51,8 @@ def variable_with_weight_loss( shape, stddev, w1 ):
         tf.add_to_collection( 'losses', weight_loss )
     return var
 
-image_holder = tf.placeholder( tf.float32, [BatchSize, ImageHeight, ImageWidth, ImageChannel] )
-label_holder = tf.placeholder( tf.int32, [BatchSize] )
+image_holder = tf.placeholder( tf.float32, [BatchSize*2, ImageHeight, ImageWidth, ImageChannel] )
+label_holder = tf.placeholder( tf.int32, [BatchSize*2] )
 
 weight1 = variable_with_weight_loss( shape = [5, 5, 3, 64 ], stddev = 5e-2, w1 = 0.0 )
 kernel1 = tf.nn.conv2d( image_holder, weight1, [ 1, 1, 1, 1 ], padding = 'SAME' )
@@ -69,7 +69,7 @@ norm2 = tf.nn.lrn( conv2, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75 )
 pool2 = tf.nn.max_pool( norm2, ksize = [ 1, 3, 3, 1 ], strides = [ 1, 2, 2, 1 ], 
                        padding = 'SAME' )
 
-reshape = tf.reshape( pool2, [ BatchSize, -1 ] )
+reshape = tf.reshape( pool2, [ BatchSize * 2, -1 ] )
 dim = reshape.get_shape()[1].value
 weight3 = variable_with_weight_loss( shape = [ dim, SampleLen ], stddev = 0.04, w1 = 0.004 )
 bias3 = tf.Variable( tf.constant( 0.1, shape = [ SampleLen ] ) )
